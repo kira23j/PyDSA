445970dc6c6d0356abc7dd7f550d67fd16f0e4eb
kira23j <kirubelmamuyesahle@gmail.com>
2024-07-03 06:12:48 -0700

    updating the repo

---
+++ 02.DesignPatterns/03.Behavioral/01.Observer/observer.py
@@ -0,0 +1,15 @@
+class Subject(object):
+    def __init__(self):
+        self._observer=[]
+        
+    def attach(self,observer):
+        
+    def detach(self,observer):
+        
+    def notify(self,observer):
+        
+class Core(Subject):
+    
+class TempViewer():
+    def update(self,subject):
+        
